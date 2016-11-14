094fefcdeeeaa9fc3bf9a7fbc3b27e76c8f321f5
Amanda Hope Harman <amandahharman@gmail.com>
2016-11-14 11:02:54 -0500

    Sets up prev and next buttons

--- myClinic/CalendarViewController.swift
+++ myClinic/CalendarViewController.swift
@@ -22,32 +22,73 @@ class CalendarViewController: UIViewController {
     @IBOutlet weak var fri: UILabel!
     @IBOutlet weak var sat: UILabel!
     
+    var presentedMonth: Int = NSCalendar.current.component(.month, from: Date())
+    var presentedYear: Int = NSCalendar.current.component(.year, from: Date())
+    var presentedDate: Date = Date()
+    let formatter = DateFormatter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        formatter.dateFormat = "yyyy MM dd"
         calendarView.dataSource = self
         calendarView.delegate = self
         calendarView.registerCellViewXib(file: "CellView")
+        self.calendarView.scrollEnabled = false
+        calendarView.scrollToDate(Date(), triggerScrollToDateDelegate: false, animateScroll: false) {
+            self.presentedDate = Date()
+            self.setupViewsOfCalendar(startDate: self.presentedDate)
+        }
     }
+    
+    @IBAction func nextButtonPressed(_ sender: UIButton) {
+        self.calendarView.scrollToNextSegment(){
+            if self.presentedMonth < 12{
+                self.presentedDate = self.formatter.date(from: "\(self.presentedYear) \(self.presentedMonth + 1) 01")!}
+            else {
+                self.presentedDate = self.formatter.date(from: "\(self.presentedYear + 1) 01 01")!
+            }
+            self.setupViewsOfCalendar(startDate: self.presentedDate)
+        }
+    }
+    
+    @IBAction func previousButtonPressed(_ sender: UIButton) {
+        self.calendarView.scrollToPreviousSegment(){
+            if self.presentedMonth > 1{
+                self.presentedDate = self.formatter.date(from: "\(self.presentedYear) \(self.presentedMonth - 1) 01")!}
+            else {
+                self.presentedDate = self.formatter.date(from: "\(self.presentedYear - 1) 12 01")!
+            }
+            self.setupViewsOfCalendar(startDate: self.presentedDate)
+        }
+
+    }
+    func setupViewsOfCalendar(startDate: Date) {
+        presentedMonth = NSCalendar.current.component(.month, from: startDate)
+        let monthName = formatter.monthSymbols[(presentedMonth-1)] // 0 indexed array
+        presentedYear = NSCalendar.current.component(.year, from: startDate)
+        monthLabel.text = monthName
+        yearLabel.text = String(presentedYear)
+    }
+
 }
 
-extension CalendarViewController: JTAppleCalendarViewDataSource{
+
+
+extension CalendarViewController: JTAppleCalendarViewDataSource, JTAppleCalendarViewDelegate{
     func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy MM dd"
-        let startDate = formatter.date(from: "2016 02 01")! // You can use date generated from a formatter
-        let endDate = Date() // You can also use dates created from this function
+        let startDate = formatter.date(from: "2016 01 01")!
+        let limitYear = NSCalendar.current.component(.year, from: Date()) + 2
+        let endDate = formatter.date(from: "\(limitYear) 01 01")!
         let parameters = ConfigurationParameters(startDate: startDate,
                                                  endDate: endDate,
-                                                 numberOfRows: 5,
+                                                 numberOfRows: 6,
                                                  calendar: Calendar.current,
                                                  generateInDates: .forAllMonths,
                                                  generateOutDates: .tillEndOfGrid,
                                                  firstDayOfWeek: .sunday)
         return parameters
     }
-}
 
-extension CalendarViewController: JTAppleCalendarViewDelegate{
     func calendar(_ calendar: JTAppleCalendarView, willDisplayCell cell: JTAppleDayCellView, date: Date, cellState: CellState) {
         let myCustomCell = cell as! CellView
         
